79d7ed8b56a149648ee1386ced5bbd2d2a85af63
Raymond <yeruiminqq@gmail.com>
2015-03-18 16:47:45 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -20,12 +20,109 @@ window.addEventListener('DOMContentLoaded', function () {
     afterRemove: function () {}
   });
 
-  $('#j-card').on('click', function () {
-    $('#j-mask').show();
+  var mask = (function () {
+    'use strict';
+
+    function mask(args) {
+      // enforces new
+      if (!(this instanceof mask)) {
+        return new mask(args);
+      }
+      // constructor body
+    }
+
+    mask.prototype = {
+      'maskEl': null,
+      'cardEl': null,
+      'closeEl': null,
+      'type': 1
+    };
+
+    mask.prototype.init = function (args) {
+
+      var _this = this;
+
+      if (args) {
+        for (var prop in args) {
+          if (args.hasOwnProperty(prop)) {
+            _this[prop] = args[prop];
+          }
+        }
+      }
+
+      if (_this.maskEl && _this.closeEl) {
+        _this.bindEvent();
+      } else {
+        return;
+      }
+
+    };
+
+    mask.prototype.bindEvent = function () {
+      var _this = this;
+
+      _this.cardEl.on('touchstart', function () {
+        _this.type = 1;
+        _this.toggleMask(1, 1);
+      });
+
+      _this.closeEl.on('touchstart', function () {
+        _this.toggleMask(0, 0);
+      });
+
+    };
+
+    mask.prototype.toggleMask = function (a, b) {
+
+      var _this = this;
+
+      if (_this.type) {
+        if (b) {
+          $('#j-mask .m-mask-main').show();
+        } else {
+          setTimeout(function () {
+            $('#j-mask .m-mask-main').hide();
+          }, 300);
+        }
+      } else {
+        if (b) {
+          $('#j-mask .m-mask-end').show();
+        } else {
+          $('#j-mask .m-mask-end').hide();
+        }
+      }
+
+      if (a) {
+        _this.cardEl.addClass('active');
+      } else {
+        _this.cardEl.removeClass('active');
+      }
+
+      if (b) {
+        _this.maskEl.addClass('active');
+      } else {
+        _this.maskEl.removeClass('active');
+      }
+
+    };
+
+    return mask;
+  }());
+
+  var Mask = new mask();
+  Mask.init({
+    maskEl: $('#j-mask'),
+    cardEl: $('#j-card'),
+    closeEl: $('.j-mask-close'),
   });
 
-  $('#j-mask-close').on('click', function () {
-    $('#j-mask').hide();
+  var game = new dpwGame();
+  game.init();
+
+  // add btn event
+  $('.j-game-start').on('touchstart', function (event) {
+    var gameType = $(this).attr('data-gametype');
+    game.start(gameType);
   });
 
 });
